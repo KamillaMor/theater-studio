b716448a796b8ba31212f60504c5a56dbf96b280
Kamil <kamilla.morozova.05@gmail.com>
2025-10-22 15:41:52 +0500

    Fix

---
+++ pages/contacts.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function Contacts() {
+  return (
+    <Layout>
+      <div style={{ padding: '4rem 2rem', textAlign: 'center' }}>
+        <h1 style={{ color: '#800020', fontSize: '3rem' }}>Контакты</h1>
+        <p style={{ fontSize: '1.2rem', color: '#333' }}>Страница в разработке</p>
+      </div>
+    </Layout>
+  );
+}
